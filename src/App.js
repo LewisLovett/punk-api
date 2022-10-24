c8f6beeafd730b240b370422d6c5d24c005f8c9f
Lewis Lovett <llovett7575@gmail.com>
2022-10-24 14:53:22 +0100

    Created search handler and beerList state

--- src/App.js
+++ src/App.js
@@ -3,17 +3,25 @@ import './App.css';
 import beers from "./data/beers";
 import NavBar from './containers/NavBar/NavBar';
 import Main from "./containers/Main/Main";
+import { useState } from 'react';
 
 function App() {
+  const [beerList, setBeerList] = useState(beers);
   const filterListArry = [{"name":"High ABV (>6.0%)", "beer_key": "abv", "beer_value" : 6},
     {"name":"Classic Range", "beer_key": "first_brewed", "beer_value" : "2010"},
     {"name":"Acidic", "beer_key": "ph", "beer_value" : 4}
   ];
 
+  const handleSearch = event => {
+    const searchTerm = event.target.value;
+    setBeerList(
+      beers.filter(beer => beer.name.includes(searchTerm))
+    )
+  }
   return (
     <div className="App">
       <NavBar filterListArry={filterListArry} />
-      <Main beerArry={beers} />
+      <Main beerArry={beerList} />
     </div>
   );
 }
