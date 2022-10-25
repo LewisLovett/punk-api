0a313a9ae4398a1d88d474f187b7a4c52fa00b62
Lewis Lovett <llovett7575@gmail.com>
2022-10-25 16:15:47 +0100

    Added API call

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import logo from './logo.svg';
 import './App.scss';
-import beers from "./data/beers";
 import NavBar from './containers/NavBar/NavBar';
 import Main from "./containers/Main/Main";
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [beerList, setBeerList] = useState(beers);
+  const [beerList, setBeerList] = useState([]);
+  const [fullBeerList, setFullBeerList] = useState([]);
   const [isHighABVFilter, setIsHighABVFilter] = useState(false);
   const [isClassicRangeFilter, setIsClassicRangeFilter] = useState(false);
   const [isAcidicFilter, setIsAcidicFilter] = useState(false);
   let currentSearchTerm = "";
+
+const getBeers = async () => {
+  const url = "https://api.punkapi.com/v2/beers";
+  const res = await fetch(url);
+  const data = await res.json();
+  setFullBeerList(data);
+  setBeerList(data);
+}
+
 
 
   const handleSearch = event => {
@@ -18,15 +27,17 @@ function App() {
     if(currentSearchTerm){
       setBeerList(beerList.filter(beer => beer.name.toLowerCase().includes(currentSearchTerm))
     )}else{
-      setBeerList(beers);
+      setBeerList(fullBeerList)
     }
   }
-useEffect(() => {
 
-})
+useEffect(() =>{
+  getBeers();
+},[])
 
 useEffect(() => {
-  let tempBeerList = beers;
+  
+  let tempBeerList = fullBeerList;
     if(isHighABVFilter){
       tempBeerList = tempBeerList.filter(beer => beer.abv>6);
     }
@@ -39,8 +50,6 @@ useEffect(() => {
     setBeerList(tempBeerList);
 }, [isHighABVFilter, isClassicRangeFilter,isAcidicFilter]);
 
-
- 
 
   const handleHighABVFilter = (event) => {
     setIsHighABVFilter(isHighABVFilter =>!isHighABVFilter);
@@ -61,11 +70,13 @@ useEffect(() => {
   {"name":"Acidic", "filter_function" : handleAcidicFilter}
 ];
   return (
+  
     <div className="App">
       <h1>Punk Beer Search</h1>
       <NavBar filterListArry={filterListArry} handleSearch={handleSearch}/>
       <Main beerArry={beerList} />
     </div>
+  
   );
 }
 
--- src/containers/CardList/CardList.jsx
+++ src/containers/CardList/CardList.jsx
@@ -2,7 +2,6 @@ import "./CardList.scss";
 import Card from "../../components/Card/Card";
 const CardList = (props) =>{
     const {beerArry} = props;
-    // const {beerName, beerTumbnail, beerDesc} = props;
     const cards = beerArry.map((beer) =>{
         return <Card beerName = {beer.name} beerThumbnail = {beer.image_url} beerDesc = {beer.description} key={beer.id} />
     })
